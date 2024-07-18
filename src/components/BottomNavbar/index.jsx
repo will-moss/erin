d0db73f4e0ee35e88b39169a54c8c2adcfcac60a
Will Moss <contact@will-moss.me>
2024-07-18 18:37:56 +0700

    feat(client): added download feature : You can now download the current video on your device

--- src/components/BottomNavbar/index.jsx
+++ src/components/BottomNavbar/index.jsx
@@ -1,16 +1,16 @@
 // Assets
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faVolumeMute, faVolumeUp } from '@fortawesome/free-solid-svg-icons';
-import './index.css';
+import { faDownload, faVolumeMute, faVolumeUp } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import "./index.css";
 
-const BottomNavbar = ({ isMuted, onToggleMute }) => {
+const BottomNavbar = ({ isMuted, onToggleMute, onDownload }) => {
   return (
     <div className="bottom-navbar">
+      <button type="button" className="nav-item" onClick={onDownload}>
+        <FontAwesomeIcon icon={faDownload} className="icon" />
+      </button>
       <button type="button" className="nav-item" onClick={onToggleMute}>
-        <FontAwesomeIcon
-          icon={!isMuted ? faVolumeUp : faVolumeMute}
-          className="icon"
-        />
+        <FontAwesomeIcon icon={!isMuted ? faVolumeUp : faVolumeMute} className="icon" />
       </button>
     </div>
   );
