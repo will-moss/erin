5f0afdd8563ed7cb0fb0c2f75e2a5d70734549c6
Will Moss <will@adriel.solutions>
2024-07-22 17:00:25 +0700

    feat(client): added blacklist feature : You can now hide videos from your feed

--- src/components/BottomNavbar/index.jsx
+++ src/components/BottomNavbar/index.jsx
@@ -1,11 +1,25 @@
 // Assets
-import { faDownload, faVolumeMute, faVolumeUp } from "@fortawesome/free-solid-svg-icons";
+import {
+  faDownload,
+  faEyeSlash,
+  faVolumeMute,
+  faVolumeUp,
+} from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import "./index.css";
+import { useLongPress } from "@uidotdev/usehooks";
 
-const BottomNavbar = ({ isMuted, onToggleMute, onDownload }) => {
+const BottomNavbar = ({ isMuted, onToggleMute, onDownload, onBlacklist, onOpenBlacklist }) => {
   return (
     <div className="bottom-navbar">
+      <button
+        type="button"
+        className="nav-item"
+        onClick={onBlacklist}
+        {...useLongPress(onOpenBlacklist, { threshold: 500 })}
+      >
+        <FontAwesomeIcon icon={faEyeSlash} className="icon" />
+      </button>
       <button type="button" className="nav-item" onClick={onDownload}>
         <FontAwesomeIcon icon={faDownload} className="icon" />
       </button>
